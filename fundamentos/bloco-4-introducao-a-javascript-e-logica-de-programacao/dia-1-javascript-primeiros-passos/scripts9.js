ea6296a4403409b8b3c743c9c7fee3d6c5451a24
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2022-06-21 19:01:31 -0300

    criado programa de ângulos e triângulos

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts9.js
@@ -0,0 +1,12 @@
+// Ângulos e triângulos
+const a = 90;
+const b = 45;
+const c = 48;
+const ang = 180;
+
+if (ang == a + b + c) {
+    console.log("É um triângulo");
+}
+else {
+    console.log("Não é um triângulo");
+}
