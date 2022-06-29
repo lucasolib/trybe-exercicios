e620ce4c3358fda5986d5451cccf2011ffc9da59
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2022-06-29 15:36:06 -0300

    Exercicios de prática

--- bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("header-container").style.backgroundColor = "#67c857";
-document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "#ffcc99";
-document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "#f7f08e";
-document.getElementsByTagName("h3")[2].style.backgroundColor = "black";
-document.getElementsByTagName("h3")[3].style.backgroundColor = "black";
-document.getElementsByTagName("h3")[0].style.backgroundColor = "#9400d3";
-document.getElementsByTagName("h3")[1].style.backgroundColor = "#9400d3";
